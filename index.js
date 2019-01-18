0add5dea2a99a05f51296afe982fea5502b6fda8
Patrick Hofmann <pm.hofmann@yahoo.co.uk>
2019-01-18 14:03:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-/**
- * Don't change these constants!
- */
-const DODGER = document.getElementById('dodger')
-const GAME = document.getElementById('game')
-const GAME_HEIGHT = 400
-const GAME_WIDTH = 400
-const LEFT_ARROW = 37 // use e.which!
-const RIGHT_ARROW = 39 // use e.which!
-const ROCKS = []
-const START = document.getElementById('start')
+const DODGER = document.getElementById('dodger');
+const GAME = document.getElementById('game');
+const GAME_HEIGHT = 400;
+const GAME_WIDTH = 400;
+const LEFT_ARROW = 37; 
+const RIGHT_ARROW = 39;
+const ROCKS = [];
+const START = document.getElementById('start');
 
 var gameInterval = null
 
